4155fa150a3fb83196f14c8fedb19597bc0ebff3
tanulo <tanulo@barossgabor.local>
2025-05-26 11:31:03 +0200

    login.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
 const btnLogin = document.querySelector('.login');
 const btnReg = document.querySelector('.reg');
 
-btnLogin.addEventListener('click', () => {
+//console.log(btnLogin, btnReg);
+
+btnLogin.addEventListener('click', ()=> {
     window.location.href = '../html/login.html';
 });
 
-function login() {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('psw').value;
-
-    console.log(email, password)
-}
+btnReg.addEventListener('click', () => {
+    window.location.href = '../html/registration.html';
+});
